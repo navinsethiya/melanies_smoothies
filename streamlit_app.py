e54d2e3dd828514e4dc4270f763b3e1faa607d10
navinsethiya <68172271+navinsethiya@users.noreply.github.com>
2025-02-10 15:01:38 +0530

    Update streamlit_app.py
    fetched  fruit column  in dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,14 @@ st.write("Name on your Smoothie will be: ",name_on_order)
 cnx=st.connection("snowflake")
 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(data=pd_df, use_container_width=True)
+# st.dataframe(data=pd_df, use_container_width=True)
 # st.stop()
+
 ingredients_list=st.multiselect(
     'choose upto 5 Ingredients: '
 ,my_dataframe,max_selections=5)
